7c56ce2078ca27aa3d19ffb4a0a13d7c0cd167dc
samuel-reedy <samreedy7@gmail.com>
2025-12-03 20:00:29 +1300

    Day 02: Passing part 2

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -18,9 +18,30 @@ def part_one(input_data):
         
     
 def part_two(input_data):
-    print("Part Two")    
+    print("Part One")
+    total = 0
+    ids = input_data.split(',')
+    for id_range in ids:
+        start_value = int(id_range.split('-')[0])
+        end_value = int(id_range.split('-')[1])
+        print(f"Processing range: {start_value}-{end_value}")
+        for value in range(start_value, end_value + 1):
+            string = str(value)
+            length = len(string)
+            for pattern_length in range(1, length):
+                if length % pattern_length == 0:
+                    invalid = True
+                    for pos in range(0, length - pattern_length, pattern_length):
+                        if string[pos:pos+pattern_length] != string[pos+pattern_length:pos+2*pattern_length]:
+                                invalid = False
+                    if invalid:
+                        print(f"Invalid ID: {value}")
+                        total += value
+                        break
+    print(f"Final total: {total}")
     
 if __name__ == "__main__":
+    
     print(f"--- Day {day} ---")
     with open(f"day_{day}.txt", "r") as file:
         input_data = file.read().strip()
