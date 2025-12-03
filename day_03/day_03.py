7e5f48c89e31b5a8f6a1509d72d4acb236cc45a0
samuel-reedy <samreedy7@gmail.com>
2025-12-03 21:06:36 +1300

    Day 03: Passing part 2

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -1,32 +1,37 @@
 day = "03"
 
+def find_joltage(banks, count):
+    banks = input_data.splitlines()
+    total = 0
+    for bank in banks:
+        batteries = []
+        for step in range(count):
+            index_end = len(bank) - (count - step) + 1
+            largest = 0
+            largest_idx = 0
+            for idx in range(0, index_end):
+                if int(bank[idx]) > largest:
+                    largest = int(bank[idx])
+                    largest_idx = idx
+            batteries.append(str(largest))
+            bank = bank[largest_idx+1:]
+        joltage = "".join(batteries)
+        total += int(joltage)
+    return total
+
 def part_one(input_data):
     print("Part One")
-    total = 0
     banks = input_data.splitlines()
-    for bank in banks:
-        largest_idx = 0
-        largest = 0
-        for idx in range(len(bank) - 1):
-            if int(bank[idx]) > largest:
-                largest_idx = idx
-                largest = int(bank[idx])
-        second_largest = 0
-        for idx in range(largest_idx+1, int(len(bank))):
-            if int(bank[idx]) > second_largest:
-                second_largest = int(bank[idx])
-            
-        joltage = str(largest) + str(second_largest)
-        total += int(joltage)
+    total = find_joltage(banks, 2)
     print(f"Final total: {total}")
+  
+  
         
-        
-                
-        
-            
-    
 def part_two(input_data):
-    print("Part Two")    
+    print("Part Two")
+    banks = input_data.splitlines()
+    total = find_joltage(banks, 12)
+    print(f"Final total: {total}")   
     
 if __name__ == "__main__":
     print(f"--- Day {day} ---")
